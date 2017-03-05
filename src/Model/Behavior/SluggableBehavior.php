9c875239872d52374c082e4e5fc3dd6689b86b70
David Běhal <behal.david@gmail.com>
2017-03-05 23:50:16 +0100

    Fixed slugable finder for translations

--- src/Model/Behavior/SluggableBehavior.php
+++ src/Model/Behavior/SluggableBehavior.php
@@ -1,8 +1,10 @@
 <?php
 namespace DejwCake\Helpers\Model\Behavior;
 
+use Cake\Core\Configure;
 use Cake\Datasource\EntityInterface;
 use Cake\Event\Event;
+use Cake\I18n\I18n;
 use Cake\ORM\Behavior;
 use Cake\ORM\Entity;
 use Cake\ORM\Query;
@@ -46,8 +48,10 @@ class SluggableBehavior extends Behavior
 
     public function findSlug(Query $query, array $options)
     {
-        return $query->where(['slug' => $options['slug']]);
-//        debug($this->_table);die;
-//        return $query->where([$this->_table->translationField('slug')  => $options['slug']]);
+        if(I18n::locale() == Configure::read('App.defaultLocale')) {
+            return $query->where(['slug' => $options['slug']]);
+        } else {
+            return $query->where([$this->_table->translationField('slug')  => $options['slug']]);
+        }
     }
 }
