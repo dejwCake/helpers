5b6dfc6930840130d1472b046916afce9424b018
David Běhal <behal.david@gmail.com>
2017-03-15 18:31:03 +0100

    Changed and moved lang middleware

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,12 +1,13 @@
 <?php
 
 use Cake\Event\EventManager;
+use Cake\Routing\Middleware\RoutingMiddleware;
 use DejwCake\Helpers\Middleware\LangFromUrlMiddleware;
 use DejwCake\Helpers\Middleware\TranslateMiddleware;
 
 EventManager::instance()->on(
     'Server.buildMiddleware',
     function ($event, $middleware) {
-        $middleware->add(new LangFromUrlMiddleware());
+        $middleware->insertBefore(RoutingMiddleware::class, new LangFromUrlMiddleware());
         $middleware->add(new TranslateMiddleware());
     });
--- src/Middleware/LangFromUrlMiddleware.php
+++ src/Middleware/LangFromUrlMiddleware.php
@@ -4,17 +4,18 @@ namespace DejwCake\Helpers\Middleware;
 
 use Cake\Core\Configure;
 use Cake\I18n\I18n;
+use Cake\Routing\Router;
 
 class LangFromUrlMiddleware
 {
     public function __invoke($request, $response, $next)
     {
         //TODO move to Standard Web
-        $params = $request->getAttribute('params', []);
-        if(!empty($params['language'])) {
+        $segments = array_filter(explode('/', urldecode($request->getUri()->getPath())));
+        if(!empty($segments[1])) {
             $supportedLanguages = Configure::read('App.supportedLanguages');
-            if (array_key_exists($params['language'], $supportedLanguages)) {
-                I18n::locale($supportedLanguages[$params['language']]['locale']);
+            if (array_key_exists($segments[1], $supportedLanguages)) {
+                I18n::locale($supportedLanguages[$segments[1]]['locale']);
             }
         }
 
